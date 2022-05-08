b6b3e415b05f96c2589fb718a875003fe350eea2
Raphael Antunes Baruffi <42071700+Baruffi@users.noreply.github.com>
2022-05-07 22:01:38 -0300

    Added BoardHelper and greatly improved retrieval performance for piece_id and piece_pos

--- src/chess/standard/board.rs
+++ src/chess/standard/board.rs
@@ -1,8 +1,113 @@
 use crate::chess::{
-    board::{Board, BoardSlice},
+    board::Board,
     piece::{Piece, PieceId, PiecePos},
 };
 use std::{collections::HashMap, marker::PhantomData};
+
+pub struct BoardHelper<const T_ROW_SIZE: usize, const T_COL_SIZE: usize, const T_BOARD_SIZE: usize>
+{
+    ids_per_position: [u64; T_BOARD_SIZE],
+    positions_per_version: HashMap<isize, Vec<usize>>,
+}
+
+impl<const T_ROW_SIZE: usize, const T_COL_SIZE: usize, const T_BOARD_SIZE: usize>
+    BoardHelper<T_ROW_SIZE, T_COL_SIZE, T_BOARD_SIZE>
+{
+    fn new<P: Piece>(initial_state: [isize; T_BOARD_SIZE]) -> Self {
+        let mut ids_per_position = [0u64; T_BOARD_SIZE];
+        let mut positions_per_version = HashMap::new();
+
+        for (pos, i) in initial_state.iter().enumerate() {
+            if i == &0 {
+                continue;
+            }
+            positions_per_version
+                .entry(*i)
+                .or_insert(Vec::new())
+                .push(pos);
+            let piece_id =
+                Self::convert_to_id::<P>(&PieceId::from((*i, positions_per_version[i].len())));
+            ids_per_position[pos] = piece_id;
+        }
+
+        BoardHelper {
+            ids_per_position,
+            positions_per_version,
+        }
+    }
+
+    fn row(&self, row: usize) -> [isize; T_ROW_SIZE] {
+        assert!(
+            row < T_ROW_SIZE,
+            "the board only has {} rows. Received index: {}",
+            T_ROW_SIZE,
+            row
+        );
+        self.ids_per_position[row * T_ROW_SIZE..row * T_ROW_SIZE + T_COL_SIZE]
+            .iter()
+            .map(|id| Self::convert_to_i(*id))
+            .collect::<Vec<isize>>()
+            .try_into()
+            .expect("unexpected slice length")
+    }
+
+    fn col(&self, col: usize) -> [isize; T_COL_SIZE] {
+        assert!(
+            col < T_COL_SIZE,
+            "the board only has {} columns. Received index: {}",
+            T_COL_SIZE,
+            col,
+        );
+        let mut array = [0; T_COL_SIZE];
+        for u in 0..T_COL_SIZE {
+            array[u] = Self::convert_to_i(self.ids_per_position[col + u * T_ROW_SIZE]);
+        }
+        array
+    }
+
+    fn convert_to_id<P: Piece>(custom_identifier: &PieceId<P>) -> u64 {
+        let (i, version) = (custom_identifier.i(), custom_identifier.version());
+        (i as i32 as u64) << 32 | version as u32 as u64
+    }
+
+    fn convert_to_i(id: u64) -> isize {
+        (id >> 32) as i32 as isize
+    }
+
+    fn convert_to_version(id: u64) -> usize {
+        id as u32 as usize
+    }
+
+    fn convert_to_piece_id<P: Piece>(id: u64) -> PieceId<P> {
+        PieceId::from((Self::convert_to_i(id), Self::convert_to_version(id)))
+    }
+
+    fn get_id(&self, pos: usize) -> u64 {
+        self.ids_per_position[pos]
+    }
+
+    fn get_pos(&self, id: u64) -> usize {
+        self.positions_per_version[&Self::convert_to_i(id)][Self::convert_to_version(id)]
+    }
+
+    fn set(&mut self, id: u64, pos: usize) {
+        assert!(
+            pos < T_BOARD_SIZE,
+            "the board only has {} squares. Received index: {}",
+            T_BOARD_SIZE,
+            pos,
+        );
+        self.ids_per_position[pos] = id;
+        self.positions_per_version
+            .entry(Self::convert_to_i(id))
+            .and_modify(|e| e[Self::convert_to_version(id)] = pos);
+    }
+
+    fn clear(&mut self) {
+        self.ids_per_position = [0; T_BOARD_SIZE];
+        self.positions_per_version = HashMap::new();
+    }
+}
 
 pub struct StandardBoard<
     const T_ROW_SIZE: usize,
@@ -10,8 +115,7 @@ pub struct StandardBoard<
     const T_BOARD_SIZE: usize,
     P: Piece,
 > {
-    state: [isize; T_BOARD_SIZE],
-    repeats: HashMap<isize, Vec<usize>>,
+    helper: BoardHelper<T_ROW_SIZE, T_COL_SIZE, T_BOARD_SIZE>,
     __: PhantomData<P>,
 }
 
@@ -19,45 +123,18 @@ impl<const T_ROW_SIZE: usize, const T_COL_SIZE: usize, const T_BOARD_SIZE: usize
     StandardBoard<T_ROW_SIZE, T_COL_SIZE, T_BOARD_SIZE, P>
 {
     pub fn new(initial_state: [isize; T_BOARD_SIZE]) -> Self {
-        let mut repeats: HashMap<isize, Vec<usize>> = HashMap::new();
-        let mut last_seen_pos: HashMap<isize, usize> = HashMap::new();
-        for (pos, id) in initial_state.iter().enumerate() {
-            if id == &0 {
-                continue;
-            }
-            match last_seen_pos.insert(*id, pos) {
-                Some(v) => {
-                    let repeat = repeats.entry(*id).or_insert(vec![v]);
-                    repeat.push(pos);
-                }
-                None => (),
-            }
-        }
         StandardBoard {
-            state: initial_state,
-            repeats,
+            helper: BoardHelper::new::<P>(initial_state),
             __: PhantomData,
         }
     }
 
     pub fn row(&self, row: usize) -> [isize; T_ROW_SIZE] {
-        assert!(row < T_ROW_SIZE, "the board only has {} rows", T_ROW_SIZE);
-        self.state[row * T_ROW_SIZE..row * T_ROW_SIZE + T_COL_SIZE]
-            .try_into()
-            .expect("unexpected slice length")
+        self.helper.row(row)
     }
 
     pub fn col(&self, col: usize) -> [isize; T_COL_SIZE] {
-        assert!(
-            col < T_COL_SIZE,
-            "the board only has {} columns",
-            T_COL_SIZE
-        );
-        let mut array = [0; T_COL_SIZE];
-        for u in 0..T_COL_SIZE {
-            array[u] = self.state[col + u * T_ROW_SIZE];
-        }
-        array
+        self.helper.col(col)
     }
 }
 
@@ -82,18 +159,8 @@ impl<const T_ROW_SIZE: usize, const T_COL_SIZE: usize, const T_BOARD_SIZE: usize
         if u > T_BOARD_SIZE {
             return None;
         }
-        let id = self.state[u];
-        self.repeats
-            .get(&id)
-            .and_then(|repeats| {
-                for (version, existing_pos) in repeats.iter().enumerate() {
-                    if &u == existing_pos {
-                        return Some(PieceId::from((id, version)));
-                    }
-                }
-                return None;
-            })
-            .or(Some(PieceId::from((id, 0))))
+        let id = self.helper.get_id(u);
+        return Some(BoardHelper::<T_ROW_SIZE, T_COL_SIZE, T_BOARD_SIZE>::convert_to_piece_id(id));
     }
 
     fn get_id_not_none(&self, pos: &PiecePos<P>) -> Option<PieceId<P>> {
@@ -112,32 +179,18 @@ impl<const T_ROW_SIZE: usize, const T_COL_SIZE: usize, const T_BOARD_SIZE: usize
         if id.is_none() {
             return None;
         }
-        self.repeats
-            .get(&id.i())
-            .and_then(|versions| {
-                versions.get(id.version()).and_then(|specific| {
-                    return Some(PiecePos(*specific, self));
-                })
-            })
-            .or_else(|| {
-                for (pos, i) in self.state.iter().enumerate() {
-                    if i == &id.i() {
-                        return Some(PiecePos(pos, self));
-                    }
-                }
-                return None;
-            })
+        let id = BoardHelper::<T_ROW_SIZE, T_COL_SIZE, T_BOARD_SIZE>::convert_to_id(id);
+        return Some(PiecePos(self.helper.get_pos(id), self));
     }
 
     fn set_square(&mut self, id: &PieceId<P>, square: usize) {
-        self.repeats
-            .entry(id.i())
-            .and_modify(|repeat| repeat[id.version()] = square);
-        self.state[square] = id.i();
+        self.helper.set(
+            BoardHelper::<T_ROW_SIZE, T_COL_SIZE, T_BOARD_SIZE>::convert_to_id(id),
+            square,
+        );
     }
 
     fn clear(&mut self) {
-        self.state = [0; T_BOARD_SIZE];
-        self.repeats = HashMap::new();
+        self.helper.clear();
     }
 }
